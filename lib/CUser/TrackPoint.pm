86016905c0238af9917ad6a7cc3c5502e173fcda
Cliff <>
2021-07-23 12:50:55 +0300

    пользовательские точки - упровление точками

--- lib/CUser/TrackPoint.pm
+++ lib/CUser/TrackPoint.pm
@@ -42,7 +42,17 @@ sub _root :
         Simple
 {
     my $user = user() || return;
+    
     my @grp = sqlSrch(pointgroup => uid => $user->{id}, sqlOrder('name'));
+    my %grp = 
+        map { $_->{list} = []; ($_->{id} => $_) }
+        @grp;
+    
+    my @pnt = sqlSrch(pointlist => uid => $user->{id}, sqlOrder('alt', 'name'));
+    foreach my $pnt (@pnt) {
+        my $grp = $grp{ $pnt->{grpid} } || next;
+        push @{ $grp->{list} }, $pnt;
+    }
     
     return
         'trackpoint',
@@ -137,7 +147,142 @@ sub grpdel :
     sqlDel(pointgroup => $grp->{id})
         || return err => 'db';
     
-    return ok => c(state => trackpoint => 'grpsetok'), redirect => '';
+    return ok => c(state => trackpoint => 'grpdelok'), redirect => '';
+}
+
+sub add :
+        Title('Добавление пользовательской точки')
+        ReturnOperation
+{
+    my $user = user() || return;
+    my $p = wparam();
+    
+    # Проверяем поля
+    my @ferr = ();
+    
+    # имя
+    my $name = $p->str('name');
+    if ($name eq '') {
+        push @ferr, name => 'empty';
+    }
+    
+    # группа
+    my $grpid = $p->uint('grpid');
+    if (!$grpid) {
+        push @ferr, grpid => 'empty';
+    }
+    elsif (!sqlSrch(pointgroup => id => $grpid, uid => $user->{id})) {
+        push @ferr, grpid => 'notfound';
+    }
+    
+    # высота
+    my $alt = $p->uint('alt');
+    if (!$p->exists('alt') || ($p->str('alt') eq '')) {
+        push @ferr, alt => 'empty';
+    }
+    elsif ($p->raw('alt') !~ /^\d+$/) {
+        push @ferr, alt => 'format';
+    }
+    
+    # ошибки ввода данных
+    if (@ferr) {
+        return err => 'input', field => { @ferr };
+    }
+    
+    # Добавляем в базу
+    my $pntid = sqlAdd(
+        pointlist =>
+        uid     => $user->{id},
+        dtadd   => Clib::DT::now(),
+        name    => $name,
+        grpid   => $grpid,
+        alt     => $alt
+    ) || return err => 'db';
+    
+    return ok => c(state => trackpoint => 'addok'), redirect => '';
+}
+
+sub set :
+        ParamCodeUInt(\&byIdPnt)
+        Title('Изменение пользовательской точки')
+        ReturnOperation
+{
+    my $pnt = shift() || return err => 'notfound';
+    my $p = wparam();
+    
+    # Проверяем поля
+    my @ferr = ();
+    my @upd = ();
+    
+    # имя
+    if ($p->exists('name')) {
+        my $name = $p->str('name');
+        if ($name eq '') {
+            push @ferr, name => 'empty';
+        }
+        if ($name ne $pnt->{name}) {
+            push @upd, name => $name;
+        }
+    }
+    
+    # группа
+    if ($p->exists('grpid')) {
+        my $grpid = $p->uint('grpid');
+        if (!$grpid) {
+            push @ferr, grpid => 'empty';
+        }
+        elsif (!sqlSrch(pointgroup => id => $grpid, uid => $pnt->{uid})) {
+            push @ferr, grpid => 'notfound';
+        }
+        if ($grpid != $pnt->{grpid}) {
+            push @upd, grpid => $grpid;
+        }
+    }
+    
+    # высота
+    if ($p->exists('alt')) {
+        my $alt = $p->uint('alt');
+        if ($p->str('alt') eq '') {
+            push @ferr, alt => 'empty';
+        }
+        elsif ($p->raw('alt') !~ /^\d+$/) {
+            push @ferr, alt => 'format';
+        }
+        if ($alt != $pnt->{alt}) {
+            push @upd, alt => $alt;
+        }
+    }
+    
+    # ошибки ввода данных
+    if (@ferr) {
+        return err => 'input', field => { @ferr };
+    }
+    
+    # Осталось ли, что сохранять
+    @upd || return error => 'nochange';
+    
+    # В базе
+    my $pntid = sqlUpd(
+        pointlist => $pnt->{id},
+        @upd,
+    ) || return err => 'db';
+    
+    return ok => c(state => trackpoint => 'setok'), redirect => '';
+}
+
+sub del :
+        ParamCodeUInt(\&byIdPnt)
+        Title('Удаление пользовательской точки')
+        ReturnOperation
+{
+    my $pnt = shift() || return err => 'notfound';
+    my $user = user() || return;
+    
+    # В базе
+    sqlDel(pointlist => $pnt->{id})
+        || return err => 'db';
+    
+    return ok => c(state => trackpoint => 'delok'), redirect => '';
 }
 
         
